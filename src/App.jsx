c3aa04ed7413b0d0b29e45f7a591edf08fdb57d7
yahya0morsy <ym60307@gmail.com>
2024-08-23 21:40:05 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import Hero from "./Hero.jsx"
+import Workout from "./workout.jsx"
+import Generator from "./Generator.jsx"
+import Chest from "./chest.jsx"
+import Shoulder from "./shoulder.jsx"
+function App() {
+  return (
+    <main className='min-h-screen flex flex-col bg-gradient-to-r
+     from-slate-800 to-slate-950 text-white text-sm '>
+      <Hero/>
+      <Generator/>
+      <Chest/>
+      <Shoulder/>
+    </main>
+  )
+}
+export default App
